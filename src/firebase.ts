3f2a91c9bfab447499e33141f5b3754f61d4f495
benjaminpenasarmiento-hub <benjaminpenasarmiento@gmail.com>
2026-08-28 08:04:31 -0500

    Initialize Firebase Authentication with Google provider support

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,12 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { initializeFirestore } from "firebase/firestore";
 import firebaseConfig from "../firebase-applet-config.json";
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
 export const db = initializeFirestore(app, {
   experimentalAutoDetectLongPolling: true,
 }, firebaseConfig.firestoreDatabaseId || undefined);
