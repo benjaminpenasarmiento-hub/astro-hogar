26bad0e58b2b6f8596c5abe7310baf2e59b16fc0
benjaminpenasarmiento-hub <benjaminpenasarmiento@gmail.com>
2026-08-29 08:01:56 -0500

    Fix Firebase config to use environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { initializeFirestore } from "firebase/firestore";
-import firebaseConfig from "../firebase-applet-config.json";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
+};
 
 const app = initializeApp(firebaseConfig);
 
@@ -9,4 +17,4 @@ export const auth = getAuth(app);
 
 export const db = initializeFirestore(app, {
   experimentalAutoDetectLongPolling: true,
-}, firebaseConfig.firestoreDatabaseId || undefined);
+});
